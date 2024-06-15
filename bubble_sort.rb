89b72f5cd24190b2c4da26a65a074b41c99211e3
Eduardo Gil <egpheel@users.noreply.github.com>
2024-06-15 23:17:36 +0100

    added total amount of passes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,8 @@
-numbers = [4, 3, 78, 2, 0, 2, 200, 1, 57, 82, 17]
+numbers = [4, 3, 78, 2, 0, 2, 200, 1, 57, 82, 17, 12, 6743, 124, 23]
 
 def bubble_sort(array_to_sort = [])
   swap_count_per_loop = -1
+  passes = 0
 
   until swap_count_per_loop == 0
     swap_count_per_loop = 0
@@ -15,9 +16,12 @@ def bubble_sort(array_to_sort = [])
       array_to_sort[i - 1] = current_number
       swap_count_per_loop += 1
     end
+
+    passes += 1
   end
 
   p array_to_sort
+  puts "Total number of passes: #{passes}"
 end
 
 bubble_sort(numbers)
