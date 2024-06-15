d1208f2a50e164ce1f7161f4618006f6f344416c
Eduardo Gil <egpheel@users.noreply.github.com>
2024-06-15 21:59:52 +0100

    added working bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,23 @@
+numbers = [4, 3, 78, 2, 0, 2, 200, 1, 57, 82, 17]
 
+def bubble_sort(array_to_sort = [])
+  swap_count_per_loop = -1
+
+  until swap_count_per_loop == 0
+    swap_count_per_loop = 0
+
+    array_to_sort.each_index do |i|
+      next unless i != 0
+      next unless array_to_sort[i] < array_to_sort[i - 1]
+
+      current_number = array_to_sort[i]
+      array_to_sort[i] = array_to_sort[i - 1]
+      array_to_sort[i - 1] = current_number
+      swap_count_per_loop += 1
+    end
+  end
+
+  p array_to_sort
+end
+
+bubble_sort(numbers)
